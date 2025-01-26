c669d6bbf3c034f746ee2b02d91ba4bd4a4f0669
KnightCedar <120373133+KnightCedar@users.noreply.github.com>
2025-01-26 19:10:14 +0100

    Update models.py

--- my-visualization-app/backend/api/models.py
+++ my-visualization-app/backend/api/models.py
@@ -6,3 +6,12 @@ class UploadedFile(models.Model):
     name = models.CharField(max_length=255)  # file name
     upload_time = models.DateTimeField(auto_now_add=True)  # upload time
     file_path = models.FileField(upload_to='uploads/')  # file path
+
+class AnalysisResult(models.Model):
+    # Save analysis consequense
+    file = models.ForeignKey(UploadedFile, on_delete=models.CASCADE)  # related file
+    columns = models.TextField()  # column name, stored in JSON
+    shape = models.CharField(max_length=50)  # shape of char
+    missing_values = models.TextField()  # Missing value statistics, stored in JSON
+    mean_values = models.TextField()  # mean value statisics, stored in JSON
+    created_at = models.DateTimeField(auto_now_add=True)  # analysis time
