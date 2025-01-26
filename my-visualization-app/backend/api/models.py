c9ca0a53b18b34ef073dcf1f71152d8997c6771b
KnightCedar <120373133+KnightCedar@users.noreply.github.com>
2025-01-26 16:44:10 +0100

    Update models.py

--- my-visualization-app/backend/api/models.py
+++ my-visualization-app/backend/api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class UploadedFile(models.Model):
+    # Save file information
+    name = models.CharField(max_length=255)  # file name
+    upload_time = models.DateTimeField(auto_now_add=True)  # upload time
+    file_path = models.FileField(upload_to='uploads/')  # file path
