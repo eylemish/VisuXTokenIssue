fadabc05b731ee3510381ec4c7144baee76b733f
KnightCedar <120373133+KnightCedar@users.noreply.github.com>
2025-01-27 17:30:46 +0100

    Add files via upload

---
+++ my-visualization-app/backend/utils/engine.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+from sklearn.decomposition import PCA
+from scipy.interpolate import interp1d
+
+class engine:
+    def __init__(self):
+        pass
+
+    # Apply PCA with given data and number of dimensions after dimensionality reduction.
+    # The default number of dimensions after dimensionality reduction is 2.
+    def apply_pca(self, data: pd.DataFrame,n_components: int = 2) -> pd.DataFrame:
+        try:
+            pca = PCA(n_components=n_components)
+            transformed_data = pca.fit_transform(data)
+            columns = [f'PC{i + 1}' for i in range(n_components)]
+            return pd.DataFrame(transformed_data, columns = columns)
+        except Exception as e:
+            raise ValueError(f"Error in PCA processing: {e}")
+
+    # Do interpolate with given data, given kind of interpolation, the number of generated data and the given range.
+    # The default generated kind is linear.
+    # The default number of generated data is 100.
+    # User is allowed to set the range of the interpolation(extrapolation).
+    # If the user doesn't set the range, the default value will be the maximum and minimum in x.
+    def interpolate(self, x: np.ndarray, y: np.ndarray, kind: str = 'linear', num_points: int = 100, min_value = None, max_value = None) -> pd.DataFrame:
+        try:
+            interpolator = interp1d(x, y, kind=kind, ill_value = "extrapolate")
+            if min_value is None:
+                min_value = np.min(x)
+            if max_value is None:
+                max_value = np.max(x)
+            x_new = np.linspace(min_value, max_value, num_points)
+            y_new = interpolator(x_new)
+            return pd.DataFrame({'x': x_new, 'y': y_new})
+        except Exception as e:
+            raise ValueError(f"Error in interpolation: {e}")
+
+    
