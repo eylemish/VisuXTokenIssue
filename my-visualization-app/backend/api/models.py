664871e0efdd0d88cb28e03ab816b885db8128d9
ysj2828428 <ysj2828428@gmail.com>
2025-02-04 20:08:28 +0100

    Update models.py

--- my-visualization-app/backend/api/models.py
+++ my-visualization-app/backend/api/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.timezone import now
 
 # Create your models here.
 class UploadedFile(models.Model):
@@ -15,3 +17,22 @@ class AnalysisResult(models.Model):
     missing_values = models.TextField()  # Missing value statistics, stored in JSON
     mean_values = models.TextField()  # mean value statisics, stored in JSON
     created_at = models.DateTimeField(auto_now_add=True)  # analysis time
+
+class AuditLog(models.Model):
+    # Model for logs, should be corrected after all actions are implemented.
+    ACTION_TYPES = [
+        ('CREATE', 'Create'),
+        ('UPDATE', 'Update'),
+        ('DELETE', 'Delete'),
+        ('VIEW', 'View'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    action_type = models.CharField(max_length=10, choices=ACTION_TYPES)
+    description = models.TextField()
+    timestamp = models.DateTimeField(default=now)
+    is_reverted = models.BooleanField(default=False)
+
+    def revert(self):
+        self.is_reverted = True
+        self.save()
