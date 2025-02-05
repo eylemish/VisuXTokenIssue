bfc89922f947fc026058800757db9ffbf08b6c19
KnightCedar <120373133+KnightCedar@users.noreply.github.com>
2025-02-05 14:00:44 +0100

    Update urls.py

--- my-visualization-app/backend/api/urls.py
+++ my-visualization-app/backend/api/urls.py
@@ -3,5 +3,6 @@ from .views import DataVisualizationView, UploadView
 
 urlpatterns = [
     path('visualize/', DataVisualizationView.as_view(), name='visualize'),
-    path("upload/", UploadView.as_view(), name="upload_csv")
+    path("upload/", UploadView.as_view(), name="upload_csv"),
+    path("add_data/", AddDataView.as_view(), name = "add_data")
 ]
