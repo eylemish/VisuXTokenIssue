299e7e1bc7e1a81ec4fe022fa398e2073ea3fe2b
JingWang <uxqwu@student.kit.edu>
2025-02-06 23:25:45 +0100

    debug

--- my-visualization-app/backend/api/views.py
+++ my-visualization-app/backend/api/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 from rest_framework.parsers import MultiPartParser
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
-from server_handler.engine import Engine
+from backend.server_handler.engine import Engine
 from django.http import JsonResponse
 import json
 import pandas as pd
