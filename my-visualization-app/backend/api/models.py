7e74ca5c8385788ec8db981e47efdc538eff61f2
KnightCedar <120373133+KnightCedar@users.noreply.github.com>
2025-02-11 00:04:36 +0100

    Update models.py

--- my-visualization-app/backend/api/models.py
+++ my-visualization-app/backend/api/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.timezone import now
-import pandas as panda
+import pandas as pd
 
 # Create your models here.
 class UploadedFile(models.Model):
