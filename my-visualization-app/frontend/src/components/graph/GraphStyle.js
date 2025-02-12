af6c69cf3b0c711f5c4f54fa9d776c5d7382abc4
slaymyway <fluttershymlp.3mlp@gmail.com>
2025-02-12 01:28:34 +0100

    fix graph style

--- my-visualization-app/frontend/src/components/graph/GraphStyle.js
+++ my-visualization-app/frontend/src/components/graph/GraphStyle.js
@@ -1,25 +1,22 @@
 class GraphStyle {
   constructor() {
-    this.colorScheme = 'blue';
-    this.markerStyle = { size: 8, color: 'blue' };
-    this.lineStyle = { width: 2, dash: 'solid' };
-    this.layoutSize = { width: 600, height: 400 };
+    this.colorScheme = "blue";
+    this.markerStyle = { size: 8, color: "blue" };
+    this.layoutSize = { width: 300, height: 150 };
+    this.margin = { t: 20, b: 20, l: 20, r: 20 };
   }
 
   getLayout() {
     return {
       width: this.layoutSize.width,
       height: this.layoutSize.height,
-      title: 'Graph Visualization',
+      title: "Graph Visualization",
+      margin: this.margin,
     };
   }
 
   getMarkerStyle() {
     return this.markerStyle;
-  }
-
-  getLineStyle() {
-    return this.lineStyle;
   }
 
   setColorScheme(colorScheme) {
@@ -31,20 +28,13 @@ class GraphStyle {
     this.markerStyle = { ...this.markerStyle, ...style };
   }
 
-  updateLineStyle(style) {
-    this.lineStyle = { ...this.lineStyle, ...style };
-  }
-
   resizeLayout(width, height) {
     this.layoutSize = { width, height };
   }
 
-
-  applyToGraph(graph) {
-    graph.style = { ...graph.style, ...this };
+  setMargins(t, b, l, r) {
+    this.margin = { t, b, l, r }; // Allow dynamic margin adjustments
   }
-
-
 }
 
 export default GraphStyle;
--- my-visualization-app/frontend/src/components/graph/VisualizationManager.js
+++ my-visualization-app/frontend/src/components/graph/VisualizationManager.js
@@ -1,5 +1,6 @@
 import Plot from "react-plotly.js";
 import Plotly from 'plotly.js-dist';
+import GraphStyle from "./GraphStyle";
 
 class VisualizationManager {
   constructor() {
@@ -133,45 +134,6 @@ class VisualizationManager {
 
     console.warn(`No matching chart type found for: ${type}`);
     return 0;
-  }
-}
-
-class GraphStyle {
-  constructor() {
-    this.colorScheme = "blue";
-    this.markerStyle = { size: 8, color: "blue" };
-    this.layoutSize = { width: 300, height: 150 };
-    this.margin = { t: 20, b: 20, l: 20, r: 20 };
-  }
-
-  getLayout() {
-    return {
-      width: this.layoutSize.width,
-      height: this.layoutSize.height,
-      title: "Graph Visualization",
-      margin: this.margin,
-    };
-  }
-
-  getMarkerStyle() {
-    return this.markerStyle;
-  }
-
-  setColorScheme(colorScheme) {
-    this.colorScheme = colorScheme;
-    this.markerStyle.color = colorScheme;
-  }
-
-  updateMarkerStyle(style) {
-    this.markerStyle = { ...this.markerStyle, ...style };
-  }
-
-  resizeLayout(width, height) {
-    this.layoutSize = { width, height };
-  }
-
-  setMargins(t, b, l, r) {
-    this.margin = { t, b, l, r }; // Allow dynamic margin adjustments
   }
 }
 
