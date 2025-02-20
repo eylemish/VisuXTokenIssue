50e9dc8231876cf0c49e55666689a61731ee4b34
JingWang <wjing8088@gmail.com>
2025-02-20 20:10:38 +0100

    fix uploading xlsx files

--- my-visualization-app/backend/api/views/upload_view.py
+++ my-visualization-app/backend/api/views/upload_view.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 
+import numpy as np
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.parsers import MultiPartParser
@@ -50,6 +51,7 @@ class UploadView(APIView):
 
             # Extract Column Names & Data Records
             features = list(df.columns)
+            df = df.replace({np.nan: None})
             records = df.to_dict(orient="records")
 
             # Stored in database Dataset
