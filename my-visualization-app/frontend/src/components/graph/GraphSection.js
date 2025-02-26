e914dcd6690d5bd7bce843e810bec240b4045b9b
slaymyway <fluttershymlp.3mlp@gmail.com>
2025-02-26 21:46:14 +0100

    dont need layer graphs anymore

--- my-visualization-app/frontend/src/components/graph/GraphSection.js
+++ my-visualization-app/frontend/src/components/graph/GraphSection.js
@@ -10,7 +10,7 @@ const visualizationManager = new VisualizationManager();
 const GraphSection = ({ updateGraphCards }) => {
     const [graphDetails, setGraphDetails] = useState([]);
     const [loading, setLoading] = useState(true);
-    const [combineGraphs, setCombineGraphs] = useState(false);
+    // const [combineGraphs, setCombineGraphs] = useState(false);
 
     const [visibleGraphs, setVisibleGraphs] = useState(() => {
         const savedState = localStorage.getItem("visibleGraphs");
@@ -115,74 +115,74 @@ const GraphSection = ({ updateGraphCards }) => {
     };
 
 
-    const handleLayerGraphs = () => {
-        setCombineGraphs((prevCombine) => {
-            const newCombineState = !prevCombine;
+    // const handleLayerGraphs = () => {
+    //     setCombineGraphs((prevCombine) => {
+    //         const newCombineState = !prevCombine;
 
-            if (newCombineState) {
-                // combine data
-                const combinedData = graphDetails
-                    .filter(graph => visibleGraphs[graph.graphId])
-                    .map((graph, index) => ({
-                        ...graph.graphScript?.data[0],
-                        name: graph.graphName,
-                        line: { color: graph.color || `hsl(${(index * 50) % 360}, 100%, 50%)` },
-                    }));
+    //         if (newCombineState) {
+    //             // combine data
+    //             const combinedData = graphDetails
+    //                 .filter(graph => visibleGraphs[graph.graphId])
+    //                 .map((graph, index) => ({
+    //                     ...graph.graphScript?.data[0],
+    //                     name: graph.graphName,
+    //                     line: { color: graph.color || `hsl(${(index * 50) % 360}, 100%, 50%)` },
+    //                 }));
 
-                const layout = {
-                    title: "Combined Graphs",
-                    xaxis: { title: graphDetails[0]?.xAxis || "X-axis" },
-                    yaxis: { title: graphDetails[0]?.yAxis || "Y-axis" },
-                    showlegend: true,
-                };
+    //             const layout = {
+    //                 title: "Combined Graphs",
+    //                 xaxis: { title: graphDetails[0]?.xAxis || "X-axis" },
+    //                 yaxis: { title: graphDetails[0]?.yAxis || "Y-axis" },
+    //                 showlegend: true,
+    //             };
 
-                updateGraphCards({ combinedGraph: { graphId: "combinedGraph", graphScript: { data: combinedData, layout } } });
+    //             updateGraphCards({ combinedGraph: { graphId: "combinedGraph", graphScript: { data: combinedData, layout } } });
 
-                // hide `GraphCard`
-                setVisibleGraphs(() => {
-                    const newState = {};
-                    graphDetails.forEach(graph => {
-                        newState[graph.graphId] = false;
-                    });
-                    localStorage.setItem("visibleGraphs", JSON.stringify(newState));
-                    return newState;
-                });
+    //             // hide `GraphCard`
+    //             setVisibleGraphs(() => {
+    //                 const newState = {};
+    //                 graphDetails.forEach(graph => {
+    //                     newState[graph.graphId] = false;
+    //                 });
+    //                 localStorage.setItem("visibleGraphs", JSON.stringify(newState));
+    //                 return newState;
+    //             });
 
-            } else {
-                // show `GraphCard`
-                updateGraphCards((prevCards) => {
-                    const newCards = { ...prevCards };
-                    delete newCards["combinedGraph"];
+    //         } else {
+    //             // show `GraphCard`
+    //             updateGraphCards((prevCards) => {
+    //                 const newCards = { ...prevCards };
+    //                 delete newCards["combinedGraph"];
 
-                    graphDetails.forEach(graph => {
-                        if (visibleGraphs[graph.graphId]) {
-                            newCards[graph.graphId] = graph;
-                        }
-                    });
+    //                 graphDetails.forEach(graph => {
+    //                     if (visibleGraphs[graph.graphId]) {
+    //                         newCards[graph.graphId] = graph;
+    //                     }
+    //                 });
 
-                    return newCards;
-                });
+    //                 return newCards;
+    //             });
 
 
-                setVisibleGraphs(() => {
-                    const newState = {};
-                    graphDetails.forEach(graph => {
-                        newState[graph.graphId] = true;
-                    });
-                    localStorage.setItem("visibleGraphs", JSON.stringify(newState));
-                    return newState;
-                });
-            }
+    //             setVisibleGraphs(() => {
+    //                 const newState = {};
+    //                 graphDetails.forEach(graph => {
+    //                     newState[graph.graphId] = true;
+    //                 });
+    //                 localStorage.setItem("visibleGraphs", JSON.stringify(newState));
+    //                 return newState;
+    //             });
+    //         }
 
-            return newCombineState;
-        });
-    };
+    //         return newCombineState;
+    //     });
+    // };
 
     return (
         <Card style={{ height: "100%", display: "flex", flexDirection: "column" }}>
-            <Button onClick={handleLayerGraphs} style={{ marginBottom: "20px" }}>
+            {/* <Button onClick={handleLayerGraphs} style={{ marginBottom: "20px" }}>
                 {combineGraphs ? "Show Individual Graphs" : "Layer Graphs"}
-            </Button>
+            </Button> */}
 
             {loading ? <Spin size="large" /> : (
                 <Row style={{ width: "100%" }}>
